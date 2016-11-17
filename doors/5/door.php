cbb4baa62cc0cc9a8ec0a80ccc28f5bb3c6c8417
Stefan Wiebe <3istee@abwesend.de>
2016-11-17 15:05:04 +0100

    Added fifth door

---
+++ doors/5/door.php
@@ -0,0 +1,24 @@
+	<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h3>Zum 5. Dezember gibt es drei Witze:</h3>
+
+		<p>
+			Meine Freundin schreite mich heute an: „Du hast mir gerade nicht mal zugehört, oder?!”
+			Ich dachte mir nur: „Was ein merkwürdiger Weg, ein Gespräch zu beginnen…”
+		</p>
+
+		<p>
+			Was bekam der Junge ohne Arme zu Weihnachten?
+			Handschuhe! Nein, keine Ahnung. Er hat es immer noch nicht ausgepackt.
+		</p>
+
+		<p>
+			Wie nennt man einen unentschlossenen japanischen Krieger?
+			Nunja.
+		</p>
+	</body>
+</html>
