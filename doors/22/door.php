661717fb564accf3c41f2c8eeb6e0d317a713402
Tobias Brauner <tibias.brauner@bi.bib.de>
2016-11-24 16:22:56 +0100

    door 22

---
+++ doors/22/door.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Christmas</title>
+    <link rel="stylesheet" href="CSS/style.css" title="no title" charset="utf-8">
+    <script src="JS/script.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <section class="xmas">
+      <canvas id="xmas"></canvas>
+    </section>
+  </body>
+</html>
