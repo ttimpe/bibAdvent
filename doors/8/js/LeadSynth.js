89a0cc325f94f5f5571ba1155d5d0d5eae93f509
tobias@tim.pe <tobias@tim.pe>
2016-11-28 15:12:43 +0100

    Music

--- doors/8/js/LeadSynth.js
+++ doors/8/js/LeadSynth.js
@@ -16,20 +16,30 @@ class LeadSynth extends Instrument {
         var tot = 1.0594630943592953;
         var freq = Math.pow(tot, n.note-49) * 440;
         console.log('freq is ' + freq);
-    /*    var osc1 = ac.createOscillator();
+        var osc1 = ac.createOscillator();
         osc1.frequency.value = freq;
+        osc1.detune.value = -10;
         osc1.type = 'sawtooth';
         var barLength = 60/player.BPM * 4;
-        osc1.connect(ac.destination);
-        osc1.start(ac.currentTime + (n.start*barLength));
-        osc1.stop(ac.currentTime + (n.start*barLength) + (n.length*barLength));
-        */
+
         var osc2 = ac.createOscillator();
         osc2.frequency.value = freq;
         osc2.type = 'triangle';
+        osc2.detune.value = 10;
         var barLength = 60/player.BPM * 4;
-        osc2.connect(ac.destination);
+
+        var g = ac.createGain();
+        g.gain.setValueAtTime(0.1, (n.start*barLength) + (n.length*barLength) - 0.05);
+        g.gain.linearRampToValueAtTime(0, (n.start*barLength) + (n.length*barLength));
+
+        g.connect(ac.destination);
+
+        osc2.connect(g);
         osc2.start(ac.currentTime + (n.start*barLength));
         osc2.stop(ac.currentTime + (n.start*barLength) + (n.length*barLength));
+
+        osc1.connect(g);
+        osc1.start(ac.currentTime + (n.start*barLength));
+        osc1.stop(ac.currentTime + (n.start*barLength) + (n.length*barLength));
     }
 }
