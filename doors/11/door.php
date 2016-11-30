abdf43fc894750b8db10f6981df407e32f1e318b
Kowalke Florian <iba3h14bko@pb.bib.de>
2016-11-30 09:18:21 +0100

    11 und 12 eingefügt

---
+++ doors/11/door.php
@@ -0,0 +1,289 @@
+<!DOCTYPE HTML>
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
+		<title>Geschenk</title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<style type="text/css">
+			body
+			{
+				background-color: #1a1a1a;
+				font-family: 'Open Sans', sans-serif;
+				background-image: url('pics/snow3.png');
+				animation: snow 5s linear infinite;
+			}
+			
+			a
+			{
+				color: #d9d9d9;
+				position: absolute;
+				bottom: 18%;
+				left: 44%;
+			}
+			
+			.wrapper
+			{
+				width: 200px;
+				height: 200px;
+				perspective: 1000px;
+				position: relative;
+				left: 40%;
+				top: 200px;
+				cursor: pointer;
+			}
+			
+			.cube
+			{
+				width: 100%;
+				height: 100%;
+				transform-style: preserve-3d;
+				position: absolute;
+				/*animation: testRotation 10s linear forwards;*/
+				transform: rotateX(-30deg) rotateY(45deg);
+			}
+			
+			.cube .front, .back, .right, .left, .bottom
+			{
+				background-color: #b30000;
+				margin: 0;
+				width: 200px;
+				height: 200px;
+				text-align: center;
+				line-height: 120px;
+				font-family: verdana;
+				font-size: 160%;
+				position: absolute;
+				border: 1px solid black;
+			}
+			
+			.top
+			{
+				margin: 0;
+				width: 200px;
+				height: 200px;
+				position: absolute;
+				transform-style: preserve-3d;
+			}
+			
+			.cube .top .topBack, .topFront, .topLeft, .topRight
+			{
+				width: 200px;
+				height: 100px;
+				margin: 0;
+				border: 1px solid black;
+				background-color: #b30000;
+				position: absolute;
+			}
+			
+			.cube .top .topLeft
+			{
+				transform-origin: bottom center;
+				transform: translateX(-100px) rotateZ(90deg) rotateX(0deg);
+			}
+			
+			.cube .top .topRight
+			{
+				background-color: #800000;
+				transform-origin: bottom center;
+				transform: translateX(100px) rotateY(180deg) rotateZ(90deg);
+				/*animation: open 5s infinite forwards;*/
+			}
+			
+			.cube .top .topBack
+			{
+				transform-origin: bottom center;
+				transform: translateY(-100px) rotateX(180deg) translateZ(1px);
+				/*animation: open 5s infinite forwards;*/
+			}
+			
+			.cube .top .topFront
+			{
+				transform-origin: bottom center;
+				transform: translateY(98px) translateZ(-1px);
+				/*animation: open 5s infinite forwards;*/
+			}
+			
+			.letter
+			{
+				background-color: #d9b38c;
+				width: 140px;
+				height: 60px;
+				padding: 10px;
+				font-family: 'Open Sans', sans-serif;
+				font-size: 70%;
+				border: 3px #bf8040 dashed;
+				transform-origin: center center;
+				transform: translateY(120px) translateX(10px) rotateX(90deg);
+			}
+			
+			.front
+			{
+				transform: rotateY(0deg) translateZ(100px);
+			}
+			
+			.back
+			{
+				background-color: #800000;
+				transform: rotateY(180deg) translateZ(100px);
+			}
+			
+			.right
+			{
+				background-color: #800000;
+				transform: rotateY(90deg) translateZ(100px);
+			}
+			
+			.left
+			{
+				transform: rotateY(-90deg) translateZ(100px);
+			}
+			
+			.top
+			{
+				transform: rotateX(90deg) translateZ(100px);
+			}
+			
+			.bottom
+			{
+				background-color: #800000;
+				transform: rotateX(-90deg) translateZ(100px);
+			}
+			
+			.open .topRight
+			{
+				animation: open_right 5s cubic-bezier(.38,.59,.27,.95) forwards;
+			}
+			
+			.open .topLeft
+			{
+				animation: open_left 5s cubic-bezier(.38,.59,.27,.95) forwards;
+			}
+			
+			.open .topBack
+			{
+				animation: open_back 5s 1.5s cubic-bezier(.38,.59,.27,.95) forwards;
+			}
+			
+			.open .topFront
+			{
+				animation: open_front 5s 1.5s cubic-bezier(.38,.59,.27,.95) forwards;
+			}
+			
+			.open .cube
+			{
+				animation: rotateCube 1s 5s linear forwards;
+			}
+			
+			.open .letter
+			{
+				animation: dropLetter 1s 6s linear forwards;
+			}
+			
+			@keyframes open_left
+			{
+				0% {transform: translateX(-100px) rotateZ(90deg) rotateX(0deg);}
+				100% {transform: translateX(-100px) rotateZ(90deg) rotateX(-160deg);}
+			}
+			
+			@keyframes open_right
+			{
+				0% {transform: translateX(100px) rotateY(180deg) rotateZ(90deg) rotateX(0deg);}
+				100% {transform: translateX(100px) rotateY(180deg) rotateZ(90deg) rotateX(160deg);}
+			}
+			
+			@keyframes open_front
+			{
+				0% {transform: translateY(100px) translateZ(-1px) rotateX(0deg);}
+				100% {transform: translateY(98px) translateZ(-1px) rotateX(-160deg);}
+			}
+			
+			@keyframes open_back
+			{
+				0% {transform: translateY(-100px) rotateX(180deg) translateZ(1px) rotateX(0deg);}
+				100% {transform: translateY(-100px) rotateX(180deg) translateZ(1px) rotateX(160deg);}
+			}
+			
+			@keyframes rotateCube
+			{
+				0% {transform: translateZ(0) rotateX(-30deg) rotateY(45deg);}
+				100% {transform: translateZ(160px) rotateX(-60deg) rotateY(45deg);}
+			}
+			
+			@keyframes snow
+			{
+				0% {background-position: 0px 0px;}
+				100% {background-position: 300px 300px;}
+			}
+			
+			@keyframes dropLetter
+			{
+				0% {transform: translateY(120px) translateX(10px) rotateX(90deg);}
+				75% {transform: translateY(-180px) translateX(10px) rotateX(90deg);}
+				100% {transform: translateY(-180px) translateX(10px) rotateX(90deg) rotateZ(45deg);}
+			}
+			
+		</style>
+		<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+		<script type="text/javascript">
+			
+			var clickOnce = true;
+			var contents = Array(5);
+			
+			function init()
+			{
+				/*var wrapper = document.getElementsByClassName('wrapper')[0];
+				
+				wrapper.style.cursor = 'pointer';
+				wrapper.onclick = function () {
+					wrapper.toggleClass("open");
+				
+				};*/
+				
+				
+				
+				$(".wrapper").click(function () {
+					
+					if(clickOnce)
+					{
+						contents[0] = 'Fröhliche Weihnachten<br>Deutsch';
+						contents[1] = 'Merry Christmas<br>Englisch';
+						contents[2] = 'Joyeux noël<br>Französisch';
+						contents[3] = 'Buon natale<br>Italienisch';
+						contents[4] = '¡Feliz navidad<br>Spanisch';
+						
+						var letter = document.getElementsByClassName('letter')[0];
+						var rand = Math.round(Math.random() * (contents.length-1));
+						letter.innerHTML = contents[rand];
+						document.getElementsByClassName('wrapper')[0].style.cursor = 'default';
+						
+						$(this).toggleClass("open");
+						clickOnce = false;
+					}
+				});
+			}
+			
+		</script>
+		
+	</head>
+	
+	<body onload="init()">
+		
+		<div class="wrapper">
+			<div class="cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="right"></div>
+				<div class="left"></div>
+				<div class="top">
+					<div class="topLeft"></div>
+					<div class="topRight"></div>
+					<div class="topFront"></div>
+					<div class="topBack"></div>
+				</div>
+				<div class="bottom"></div>
+				<div class="letter"></div>
+			</div>
+		</div>
+		<a>Click the box!</a>
+	</body>
+</html>
---
+++ doors/12/door.php
@@ -0,0 +1,191 @@
+<!DOCTYPE HTML>
+<html lang="de">
+	<head>
+		<title>Schneemann</title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<style type="text/css">
+			body
+			{
+				background-color: #002b80;
+				/*background-color: #1a1a1a;*/
+				background-image: url('pics/snow1.png'), url('pics/snow2.png'), url('pics/snow3.png');
+				animation: snow 10s linear infinite;
+				margin: 0;
+				padding: 0;
+			}
+			
+			h1
+			{
+				font-family: 'Open Sans', sans-serif;
+			}
+			
+			#schneemann
+			{
+				transform-origin: bottom center;
+				animation: move 10s linear infinite;
+			}
+			
+			@keyframes move
+			{
+				0% {transform: rotate(0deg);}
+				25% {transform: rotate(10deg);}
+				50% {transform: rotate(0deg);}
+				75% {transform: rotate(-10deg);}
+				100% {transform: rotate(0deg);}
+			}
+			
+			@keyframes snow
+			{
+				0% {background-position: 0px 0px;}
+				100% {background-position: 500px 500px, 400px 400px, 300px 300px;}
+			}
+			
+			#headline
+			{
+				margin-top: 100px;
+				font-size: 240%;
+				font-family: verdana, arial;
+				color: #cc0000;
+			}
+			
+			#schneemann
+			{
+				bottom: 140px;
+			}
+			
+			@media (max-width: 1500px)
+			{
+				#schneemann
+				{
+					bottom: 20px;
+				}
+			}
+			
+			@media (max-width: 500px)
+			{
+				#schneemann
+				{
+					bottom: 5px;
+				}
+			}
+			
+		</style>
+	</head>
+	
+	<body>
+		<h1 id="headline" align="center">Merry Christmas</h1>
+		
+		
+		
+		
+		<xml version="1.0" encoding="utf-8">
+<!-- Generator: Adobe Illustrator 20.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 960 560" style="enable-background:new 0 0 960 560; position: absolute; bottom: 0;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:url(#SVGID_1_);}
+	.st1{fill:url(#SVGID_2_);}
+	.st2{fill:url(#SVGID_3_);}
+</style>
+<g id="Ebene_1">
+	<g>
+		<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="130.8684" y1="431.3158" x2="130.8684" y2="658.2895">
+			<stop  offset="0" style="stop-color:#FFFFFF"/>
+			<stop  offset="0.7783" style="stop-color:#999999"/>
+		</linearGradient>
+		<ellipse class="st0" cx="130.9" cy="544.8" rx="277" ry="113.5"/>
+	</g>
+</g>
+<g id="Ebene_3">
+	<linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="805.7763" y1="422.3158" x2="805.7763" y2="617.0526">
+		<stop  offset="0" style="stop-color:#FFFFFF"/>
+		<stop  offset="0.7783" style="stop-color:#999999"/>
+	</linearGradient>
+	<ellipse class="st1" cx="805.8" cy="519.7" rx="247.7" ry="97.4"/>
+</g>
+<g id="Ebene_2">
+	<linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="456.1579" y1="431.3158" x2="456.1579" y2="676">
+		<stop  offset="0" style="stop-color:#FFFFFF"/>
+		<stop  offset="0.7783" style="stop-color:#999999"/>
+	</linearGradient>
+	<ellipse class="st2" cx="456.2" cy="553.7" rx="354.8" ry="122.3"/>
+</g>
+</svg>
+		
+                                                                                   
+                                                                                                    
+                                                                                                    
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                      
+                                                                                                                                                               
+                                                                                           
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                           
+
+	
+	<svg width="10%" id="schneemann" height="213" id="snowman" style="min-width: 200px; position: absolute; left: 40%; ">
+		<defs>
+    <style>
+      .cls-1, .cls-2, .cls-3, .cls-4, .cls-5 {
+        fill-rule: evenodd;
+      }
+
+      .cls-1 {
+        fill: url(#linear-gradient);
+      }
+
+      .cls-2 {
+        fill: url(#linear-gradient-2);
+      }
+
+      .cls-4 {
+        fill: #f39200;
+      }
+
+      .cls-5 {
+        fill: #e30613;
+      }
+    </style>
+    <linearGradient id="linear-gradient" x1="527.405" y1="768.313" x2="494.345" y2="666.563" gradientUnits="userSpaceOnUse">
+      <stop offset="0" stop-color="#bcbcbc"/>
+      <stop offset="0.584" stop-color="#fff"/>
+      <stop offset="1" stop-color="#fff"/>
+    </linearGradient>
+    <linearGradient id="linear-gradient-2" x1="517.489" y1="689.438" x2="489.292" y2="602.656" xlink:href="#linear-gradient"/>
+  </defs>
+  <path class="cls-1" d="M510.878,666.556A50.879,50.879,0,1,1,460,717.434,50.878,50.878,0,0,1,510.878,666.556Z" transform="translate(-460 -602.656)"/>
+  <path class="cls-2" d="M503.386,602.669A43.387,43.387,0,1,1,460,646.056,43.386,43.386,0,0,1,503.386,602.669Z" transform="translate(-460 -602.656)"/>
+  <path class="cls-3" d="M496.525,634.7c3.934,0,7.124,2.55,7.124,5.7s-3.19,5.695-7.124,5.695-7.125-2.55-7.125-5.695S492.59,634.7,496.525,634.7Z" transform="translate(-460 -602.656)"/>
+  <path class="cls-3" d="M527.165,626.291c3.935,0,7.124,2.559,7.124,5.716s-3.189,5.716-7.124,5.716-7.124-2.559-7.124-5.716S523.23,626.291,527.165,626.291Z" transform="translate(-460 -602.656)"/>
+  <path id="Polygon_1" data-name="Polygon 1" class="cls-4" d="M521.558,644.966l18.589,27.951-25.778-15.089Z" transform="translate(-460 -602.656)"/>
+  <path id="Scarf" class="cls-5" d="M468.742,679.333c-2.127,5.963-4.4,5.471,4.708,10.088s23.964,11.127,30.934,9.415,28.5-5.855,32.952-7.4c2.062-.713,15.895-4.037,8.07-11.432s-6.838-8.679-10.759-5.38-5.758,9.334-15.468,10.087a220,220,0,0,1-25.554,0C492.6,684.542,470.87,673.371,468.742,679.333Z" transform="translate(-460 -602.656)"/>
+  <path id="Scarf2" class="cls-5" d="M513.127,693.456c2.888,10,4.5,21.271,5.38,14.122s11.557,11.872,8.742-3.362-3.246-12.669,0-16.14" transform="translate(-460 -602.656)"/>
+	
+	<defs>
+    <style>
+      .cls-1, .cls-2 {
+        fill-rule: evenodd;
+      }
+
+      .cls-1 {
+        fill: url(#linear-gradient);
+      }
+
+      .cls-2 {
+        fill: url(#linear-gradient-2);
+      }
+	  
+
+	  
+    </style>
+    
+	
+	</svg>
+	
+	
+
+	</body>
+</html>
