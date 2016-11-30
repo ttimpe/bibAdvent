5475e9662e172244d9f2f889e3249a7a81977bc8
Mussin Ruslan <iba3h14bmu@pb.bib.de>
2016-11-30 09:09:10 +0100

    Door 3 added

---
+++ doors/3/door.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Chack-Chack</title>
+    <style type="text/css">
+
+    body{display: flex; flex-direction: column;}
+
+    table,tr,td{border:1px solid grey;}
+
+    table{margin:auto; width: 35%;}
+
+    td{width: 50%;}
+
+    h1,h2,h3{text-align: center;}
+        
+    ol {
+        padding-left: 20px;
+    }
+
+    #zubereitung {
+        width: 35%;
+        margin: 0 auto;
+    }
+
+    #chak{height: 50vh;
+          margin: auto;}
+
+    </style>
+
+  </head>
+  <body>
+
+    <div>
+    <h1>Chak-Chak Rezept</h1>
+    </div>
+
+    <table>
+    <tr><td>17 g</td><td>Butter</td></tr>
+    <tr><td>80 ml</td><td>Milch</td></tr>
+    <tr><td>5x</td><td>Eier</td></tr>
+    <tr><td>1 kg</td><td>Mehl</td></tr>
+    <tr><td>15 g</td><td>Salz</td></tr>
+    <tr><td>50 g</td><td>Zucker</td></tr>
+    <tr><td>1 kg</td><td>Butterschmalz zum Frittieren</td></tr>
+    </table>
+    <div>
+    <h2>Zutaten für den Sirup</h2>
+    </div>
+    <table>
+    <tr><td>150 g</td><td>Honig</td></tr>
+    <tr><td>2/3 Tassen</td><td>Zucker</td></tr>
+    </table>
+    <div>
+    <h2>Zubereitung</h2>
+      <div id="zubereitung">
+      <ol>
+          <li>Für den Teig die Eier trennen, die Eigelbe mit Zucker zerreiben bis die Masse weiß wird. Eiweiße steif schlagen.</li>    
+          <li>Die Eigelbe mit dem Eiweiß verrühren, gut vermischen. Salz mit Mehl vermischen, in die Eiermilchmasse zugeben, zu einem
+              festen Teig verarbeiten. Den Teig mit einem feuchten Geschirrtuch zudecken und 15-20 Min. ruhen lassen.  
+          <li>Teig ausrollen und in dünne Nudeln schneiden.</li>   
+          <li>Den Butterschmalz erhitzen und die Nudeln darin goldbraun braten. Das Fett abtropfen und die Nudeln abkühlen lassen.</li>   
+          <li>Für die Sirupzubereitung in einem Kochtopf den Honig erhitzen, den Zucker hinzugeben,gut umrühren und zum Kochen bringen.Den Schaum abschöpfen und den Sirup paar Minuten kochen lassen bis der Zucker                       vollständig aufgelöst ist.</li>
+          <li>Für die Mohn-Nuss-Mischung den Mohn mit dem kochenden Wasser für 3 Min. einweichen, dann in einem Mörser zerdrücken.       
+              Mit zerkleinerten Nüssen vermischen und in einer zugedeckten Schüssel stehen lassen.(Optional)</li>      
+              <li>Die gebratenen Nudeln in noch heißen Sirup geben, vermischen und die Mohn-Nuss-Mischung hinzugeben.
+              Den fertigen Chak-Chak in einen befetteten tiefen Teller geben, mit feuchten Händen zusammendrücken und ein Häufchen formen.</li>     
+          <li>Abkühlen lassen und in große Stücke schneiden.</li>
+      </ol>
+      </div>
+    </div>
+    
+
+  </body>
+</html>
+
+
+
