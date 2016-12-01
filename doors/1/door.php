51a92a29bcecacee61789ac145756e8058ec1b85
Tobias Timpe <tobias@tim.pe>
2016-12-01 02:17:04 +0100

    Totally didn't replace door 1 in flight. Naw, never. It looked so amazing.

--- doors/1/door.php
+++ doors/1/door.php
@@ -17,100 +17,110 @@ body{
   flex-direction:row;
   justify-content:space-around;
   padding:2%;
-  background-color: rgba(240,240 ,240 , .3);
+  background-color: #fff;
 }
-    table,td,th{
-      border:1px solid black;
 
-
-    }
-    table{
-
-      height:60vh;
-      width:30vw;
-
-
-    }
-
-    td,th{
-      text-align: center;
-      width:50%;
-
-    }
-	#Zubereitung
-	{
-
-	width:30vw;
-	text-align:left;
-
-	}
-	#ecke{
-	  width: 100%;
-	  height: auto;
-      border: 1px solid grey;
-	}
-  .beispiel{
+main {
+    width: 1024px;
+    height: 768px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-left: -512px;
+    margin-top: -384px;
     display: flex;
-    width: 30%;
+    background: #ffffff;
+    background: -moz-linear-gradient(left, #ffffff 0%, #f7f7f7 40%, #e5e5e5 47%, #d3d3d3 50%, #e5e5e5 52%, #f7f7f7 60%, #ffffff 100%);
+    background: -webkit-linear-gradient(left, #ffffff 0%,#f7f7f7 40%,#e5e5e5 47%,#d3d3d3 50%,#e5e5e5 52%,#f7f7f7 60%,#ffffff 100%);
+    background: linear-gradient(to right, #ffffff 0%,#f7f7f7 40%,#e5e5e5 47%,#d3d3d3 50%,#e5e5e5 52%,#f7f7f7 60%,#ffffff 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#ffffff',GradientType=1);
+    box-shadow: 15px 15px 30px rgba(0,0,0,0.3), 0 0 10px rgba(0,0,0,0.2);
+
+}
+main .page {
+    width: 45%;
     height: 100%;
+    display: flex;
     flex-direction: column;
-  }
-  h3 {
+    line-height: 1.7em;
+    font-family: 'Georgia';
+    margin: 0 5%;
+}
+
+h1 {
+    margin-top: 50px;
+}
+
+h2 {
+    font-size: 1.3em;
+}
+h3 {
+    font-size: 1.1em;
     margin:0;
-    text-align: center; }
+}
+main .page p {
+    text-align: justify;
+}
+img {
+    width: 100%;
+    height: auto;
+}
+ul {
+    list-style-type: square;
+}
 
 @media screen and (max-height:640px){
   body {
     flex-direction: column;
   }
 
-  table {
-    width: 98vw;
-  }
 
-  #Zubereitung {
-    width: 100vw;
-  }
 }
 
 
     </style>
   </head>
   <body>
-<div id="Zubereitung">
-	Teig: Aus den Zutaten einen Teig herstellen. Auf einem Backblech ausrollen.<br>
-	2 EL Aprikosenmarmelade erhitzen und auf den Teig streichen.<br>
-Belag: Butter, Zucker, Vanillezucker und Wasser in einem Topf aufkochen.<br>
-gemahlene Nüsse mit in den Topf geben und zu einer glatten Masse verrühren.<br>
- Die Masse auf dem Teig verteilen. Bei 160°-180° 30 Minuten backen.<br>
- Den noch warmen Kuchen in Dreiecke schneiden.<br>
- Nussecken abkühlen lassen und mit Kuvertüre bestreichen oder Tauchen.<br>
-</div>
-    <table>
-      <tr><th>Menge</th><th>Zutat</th></tr>
-      <tr><td></td><th>Für den Teig :</th></tr>
-      <tr><td>160 g</td><td>Butter</td></tr>
-      <tr><td>300 g</td><td>Mehl</td></tr>
-      <tr><td>1 Teelöffel</td><td>Backpulver</td></tr>
-      <tr><td>130 g</td><td>Zucker</td></tr>
-      <tr><td>3</td><td>Eier</td></tr>
-      <tr><td>   </td><th>Zum Bestreichen:</th></tr>
-      <tr><td>2 Esslöffel</td><td>z.B. Aprikosenkonfitüre</td></tr>
-      <tr><td></td><th>Für den Belag:</th></tr>
-      <tr><td>200 g</td><td>Butter</td></tr>
-      <tr><td>200 g</td><td>Zucker</td></tr>
-      <tr><td>2 Päckchen</td><td>Vanillezucker</td></tr>
-      <tr><td>4 Esslöffel</td><td>Wasser</td></tr>
-      <tr><td>400 g</td><td>gemahlene Nüsse</td></tr>
-      <tr><td></td><td>Kuvertüre</td></tr>
+     <main>
+    <section class="page">
+        <h1>Nussecken</h1>
+        <h2>Zutaten:</h2>
+        <h3>Für den Teig:</h3>
+        <ul>
+            <li>160g Butter</li>
+            <li>300g Mehl</li>
+            <li>1 TL Backpulver</li>
+            <li>130g Zucker</li>
+            <li>3 Eier</li>
+        </ul>
+        <h3>Für den Belag:</h3>
+        <ul>
+            <li>200g Butter</li>
+            <li>200g Zucker</li>
+            <li>2 Päckchen Vanillezucker</li>
+            <li>4 EL Wasser</li>
+            <li>400g gemahlene Nüsse</li>
+            <li>Kuvertüre</li>
+        </ul>
+        <h3>Zum Bestreichen:</h3>
+        <ul>
+            <li>2 EL z.B. Aprikosenkonfitüre</li>
+        </ul>
+    </section>
+    <section class="page">
+        <img src="pictures/nussecke.png">
 
-    </table>
-<div class="beispiel">
-  <h3>Serviervorschlag</h3>
-  <img src="pictures/Nussecke.jpg" id="ecke">
-</div>
-
-
-
+        <h2>Zubereitung</h2>
+        <h3>Teig:</h3>
+        <p>
+        Aus den Zutaten einen Teig herstellen. Auf einem Backblech ausrollen.<br>
+    	2 EL Aprikosenmarmelade erhitzen und auf den Teig streichen.<br>
+        </p>
+    <h3>Belag:</h3>
+    <p>
+    Butter, Zucker, Vanillezucker und Wasser in einem Topf aufkochen. Gemahlene Nüsse mit in den Topf geben und zu einer glatten Masse verrühren. Die Masse auf dem Teig verteilen.<br>Bei 160-180°C 30 Minuten backen. Den noch warmen Kuchen in Dreiecke schneiden. Nussecken abkühlen lassen und mit Kuvertüre bestreichen oder Tauchen.<br>
+    </p>
+ </section>
+ </main>
   </body>
 </html>
