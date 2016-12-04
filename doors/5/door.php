0283500d8db27db15c1a4761b40a5551b0c9c51f
StW96 <3istee@abwesend.de>
2016-12-04 18:35:58 +0100

    Door 5 and 6 edited.

--- doors/5/door.php
+++ doors/5/door.php
@@ -1,28 +1,46 @@
+<!DOCTYPE html>
 <?php
-define('DOOR', 5);
+define('DOOR', 1);
 include(dirname(__DIR__).'../../verify.php');
 ?>
-<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8"/>
+
+		<style type="text/css">
+			body {
+				display: flex;
+				justify-content: center;
+
+				background: hsl(350, 70%, 30%);
+
+				color: hsl(0, 0%, 90%);
+				font-family: Georgia, serif;
+			}
+
+			.content {
+				display: inline-block;
+			}
+		</style>
 	</head>
 	<body>
-		<h3>Zum 5. Dezember gibt es drei Witze:</h3>
+		<div class="content">
+			<h3>Zum 5. Dezember gibt es drei Witze:</h3>
 
-		<p>
-			Meine Freundin schrie mich heute an: „Du hast mir gerade nicht mal zugehört, oder?!”
-			Ich dachte mir nur: „Was ein merkwürdiger Weg, ein Gespräch zu beginnen…”
-		</p>
+			<p>
+				Meine Freundin schrie mich heute an: „Du hast mir gerade nicht mal zugehört, oder?!”<br>
+				Ich dachte mir nur: „Was ein merkwürdiger Weg, ein Gespräch zu beginnen…”
+			</p>
 
-		<p>
-			Was bekam der Junge ohne Arme zu Weihnachten?
-			Handschuhe! Nein, keine Ahnung. Er hat es immer noch nicht ausgepackt.
-		</p>
+			<p>
+				Was bekam der Junge ohne Arme zu Weihnachten?<br>
+				Handschuhe! Nein, keine Ahnung. Er hat es immer noch nicht ausgepackt.
+			</p>
 
-		<p>
-			Wie nennt man einen unentschlossenen japanischen Krieger?
-			Nunja.
-		</p>
+			<p>
+				Wie nennt man einen unentschlossenen, japanischen Krieger?<br>
+				Nunja.
+			</p>
+		</div>
 	</body>
 </html>
--- doors/6/door.php
+++ doors/6/door.php
@@ -1,16 +1,55 @@
+<!DOCTYPE html>
 <?php
-define('DOOR', 6);
+define('DOOR', 1);
 include(dirname(__DIR__).'../../verify.php');
 ?>
-<p>Drei Männer stehen in der Schlange zum Himmel.</p>
+<html>
+	<head>
+		<meta charset="utf-8"/>
 
-<p>Zum ersten sagt Peter: „Du betrogst deine Frau nie, du darfst mit einem Ferrari im Himmel rumfahren!”, <br>so steigt er in das Auto und fährt durch die Tore.</p>
+		<style type="text/css">
+			body {
+				display: flex;
+				justify-content: center;
 
-<p>Er ruft den zweiten und tadelt: “Oh nein, <em>zwei mal</em> hast du deine Frau betrogen! Du wirst in einem Accord fahren!”, <br>so steigt er in das Auto und fährt durch die Tore.</p>
+				background: hsl(350, 70%, 30%);
 
-<p>Zuletzt ruft er den Dritten und spricht empört: “<em>Fünf mal</em> hast du sie betrogen! Du wirst in einem Ford Pinto fahren!”</p>
+				color: hsl(0, 0%, 90%);
+				font-family: Georgia, serif;
+			}
 
-<p>Doch wenn der Dritte durch die Tore fährt sieht er den Ersten im Ferrari am Straßenrand, wie er weint.<br>
-Er fragt wundernd: “Warum weinst du? Du hast das beste Auto von uns Allen!”<br>
-Leise wimmelt er: “Meine Frau fuhr gerade auf einem Skateboard vorbei.”<br>
-</p>
+			.content {
+				display: inline-block;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="content">
+			<p>Drei Männer standen in der Warteschlange zum Himmel, wartend auf St. Peters Beurteilung.</p>
+
+			<p>
+				Den ersten lobte St. Peter:<br>
+				„Du betrogst deine Frau nie, du darfst mit einem Ferrari im Himmel rumfahren!”.<br>
+				So stieg er in den Ferrari und fuhr durch die Tore.
+			</p>
+
+			<p>
+				Tadelnd jedoch begegnete er dem zweiten:<br>
+				“Oh nein, <em>zwei mal</em> hast du deine Frau betrogen! Du wirst in einem Accord fahren!”.<br>
+				So stieg er in den Accord und fuhr durch die Tore.
+			</p>
+
+			<p>
+				Zuletzt rief er den dritten und empörte sich:<br>
+				“<em>Fünf mal</em> hast du sie betrogen!<br>
+				Bei Gott, du wirst in einem Ford Pinto fahren!”
+			</p>
+
+			<p>
+				Doch wenn der dritte durch die Tore fuhr, sah er den ersten weinend im Ferrari am Straßenrand sitzen.<br>
+				Er fragte wundernd: “Warum weinst du? Du hast das beste Auto von uns Allen!”<br>
+				Leise wimmelte er: “Meine Frau fuhr gerade auf einem Skateboard vorbei!”<br>
+			</p>
+		</div>
+	</body>
+</html>
