7d70849a08833dff57f83524eb470b996332b83a
Georgi Reger <Georgi.Reger@bi.bib.de>
2016-12-12 17:15:09 +0100

    Signed-off-by: Georgi Reger <Georgi.Reger@bi.bib.de>

--- doors/15/door.php
+++ doors/15/door.php
@@ -1,51 +1,59 @@
-<?php
-define('DOOR', 14);
-include(dirname(__DIR__).'../../verify.php');
-?>
 <!doctype html>
 
 <html lang="en">
 <head>
   <meta charset="utf-8">
 
-  <link rel="stylesheet" href="css/door14.css">
+  <link rel="stylesheet" href="css/door15.css">
 </head>
 
 <body>
-  <script src="js/door14.js"></script>
+  <script src="js/door15.js"></script>
   <canvas id="canvas"></canvas>
   <div id="text">
-  <h1>Die Weihnachtsfahrt</h1>
-<p>Denkt Euch, ich habe das Christkind gesehen,
-ich hab's überfahren, es war ein Versehen.
-Ich hatte gerade die Äuglein zu,
-ich träumte beim Fahren in himmlischer Ruh.</p>
+	<h1>Die Weihnachtsfahrt</h1>
+	<p>
+		Denkt Euch, ich habe das Christkind gesehen,<br />
+		ich hab's überfahren, es war ein Versehen.<br />
+		Ich hatte gerade die Äuglein zu,<br />
+		ich träumte beim Fahren in himmlischer Ruh.
+	</p>
 
-<p>Das Christkind hat in dieser heiligen Nacht
-Bekanntschaft mit meinem Kühler gemacht.
-Später sah ich auch noch den Weihnachtsmann,
-er feuerte grad' seine Renntiere an.</p>
+	<p>
+		Das Christkind hat in dieser heiligen Nacht<br />
+		Bekanntschaft mit meinem Kühler gemacht.<br />
+		Später sah ich auch noch den Weihnachtsmann,<br />
+		er feuerte grad' seine Renntiere an.<br />
+	</p>
 
-<p>Ich überholte den langsamen Wicht,
-doch sah ich den Gegenverkehr dabei nicht.
-Ich wich noch aus, doch leider nicht Santa,
-ein kurzes Rumsen und er klebte am Manta.</p>
+	<p>
+		Ich überholte den langsamen Wicht,<br />
+		doch sah ich den Gegenverkehr dabei nicht.<br />
+		Ich wich noch aus, doch leider nicht Santa,<br />
+		ein kurzes Rumsen und er klebte am Manta.<a>
+	</p>
 
-<p>Am Ende sah ich auch den Nikolaus,
-er stürmte gerade aus dem Freudenhaus.
-Er kam ganz hektisch über die Kreuzung gelaufen,
-wollt am Automaten neue Präser sich kaufen.</p>
+	<p>
+		Am Ende sah ich auch den Nikolaus,<br />
+		er stürmte gerade aus dem Freudenhaus.<br />
+		Er kam ganz hektisch über die Kreuzung gelaufen,<br />
+		wollt am Automaten neue Präser sich kaufen.<br />
+	</p>
 
-<p>Mein Auto und mich hat er wohl nicht gesehen,
-jedenfalls blieben von ihm nur die Stiefel stehen.
-So ist die Moral von dem Gedicht,
-Fahr zu schnell Dein Auto nicht.</p>
+	<p>
+		Mein Auto und mich hat er wohl nicht gesehen,<br />
+		jedenfalls blieben von ihm nur die Stiefel stehen.<br />
+		So ist die Moral von dem Gedicht,<br />
+		Fahr zu schnell Dein Auto nicht.<br />
+	</p>
 
-<p>Denn als ich zuhause war, da musste ich heulen,
-mein schöner Wagen, der hatte drei Beulen,
-vom Christkind, vom Niklas und vom Santa Claus,
-ja, dieses Jahr fällt Weihnachten dann wohl aus!</p>
-<p id= "autor">-Author: Unbekannt</p>
+	<p>
+		Denn als ich zuhause war, da musste ich heulen,<br />
+		mein schöner Wagen, der hatte drei Beulen,<br />
+		vom Christkind, vom Niklas und vom Santa Claus,<br />
+		ja, dieses Jahr fällt Weihnachten dann wohl aus!<br />
+	</p>
+	<p id= "autor">-Author: Unbekannt</p>
   </div>
 </body>
 </html>
