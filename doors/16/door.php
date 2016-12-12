4ab8fe7823e54a788c4905f0628585aebbe9a2ba
Georgi Reger <Georgi.Reger@bi.bib.de>
2016-12-12 17:24:00 +0100

    Doors #15 and #16 have arrived

---
+++ doors/16/door.php
@@ -0,0 +1,38 @@
+<!doctype html>
+
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+
+  <link rel="stylesheet" href="css/door16.css">
+</head>
+
+<body>
+  <script src="js/door16.js"></script>
+	<div class="message">
+		<h1>Happy Holidays!</h1>
+	</div>
+<div class="window">
+  <canvas id="canvas"></canvas>
+		<div class="santa">
+			<div class="head">
+				<div class="face">
+					<div class="redhat">
+						<div class="whitepart"></div>
+						<div class="redpart"></div>
+						<div class="hatball"></div>
+					</div>
+					<div class="eyes"></div>
+					<div class="beard">
+						<div class="nouse"></div>
+						<div class="mouth"></div>
+					</div>
+				</div>
+				<div class="ears"></div>
+			</div>
+			<div class="body"></div>
+		</div>
+	</div>
+</body>
+</html>
+<a href='http://www.freepik.com/free-vector/christmas-decoration-red-background_973499.htm'>Background by Freepik</a>
