78945db85995f1a1ef443119e175c442f7e71282
Reger Georgi <iba3h14bre@pb.bib.de>
2016-12-14 07:41:28 +0100

    nearly forgot about JQuery

--- doors/14/door.php
+++ doors/14/door.php
@@ -4,9 +4,7 @@
         <meta charset="utf-8" />
         <title>SAVE THE XMAS!</title>
         <link rel="stylesheet" href="main.css">
-        <!--
         <script src="//code.jquery.com/jquery.min.js"></script>
-        -->
         <script type="text/javascript" src="js/iframe.js"></script>
         <script type="text/javascript" src="js/phaser.min.js"></script>
         <script type="text/javascript" src="js/game.js"></script>
