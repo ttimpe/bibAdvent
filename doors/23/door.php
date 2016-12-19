8277f74854147a8d5b98987a88901fe421d58d31
Georgi Reger <Georgi.Reger@bi.bib.de>
2016-12-19 19:22:23 +0100

    Die Putenschnitzel abtupfen mit Küchenkrepp, salzen, pfeffern und mit gemahlenem Zimt würzen. Danach mehlieren und in dem aufgeschlagenen Ei wälzen. Dann in die Panade geben, welche gemischt wird zu gleichen Teilen aus Semmelmehl, Parmesankäse (gerieben) und Haselnussmehl. 			Wer will, kann sie noch mit Rosenpaprika würzen. Die panierten Schnitzel in das heiße Butterschmalz geben und von beiden Seiten schön goldgelb anbraten.

---
+++ doors/23/door.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>23. Türchen</title>
+		<link rel="stylesheet" type="text/css" href="tuerchen23.css"/>
+	</head>
+	<body>
+		<h1>1.	Weihnachtsschnitzel</h1>
+		<h2>Zutaten</h2>
+		<p><span style="width:50px;display:block;float:left;">4</span> mittelgroße	Putenschnitzel<br/>
+		   <span style="width:50px;display:block;float:left;">1</span> 	Ei<br/>
+		   <span style="width:50px;display:block;float:left;">25 g</span>	Semmelbrösel<br/>
+		   <span style="width:50px;display:block;float:left;">25 g</span>	Parmesan<br/>
+		   <span style="width:50px;display:block;float:left;">25 g</span>	Haselnüsse, gemahlen<br/>
+		   <span style="width:50px;display:block;float:left;">20 g</span>	Mehl<br/>
+			Salz und Pfeffer<br/>
+			Zimtpulver<br/>
+		   <span style="width:50px;display:block;float:left;">evtl.</span>	Paprikapulver<br/>
+			Butterschmalz, nach Bedarf<br/></p>
+		<h2>Zubereitung</h2>	
+		<p>Arbeitszeit: ca. 30 Min.
+			<br/>
+			<br/>
+			Die Putenschnitzel abtupfen mit Küchenkrepp, salzen, pfeffern und mit gemahlenem Zimt würzen. Danach mehlieren und in dem aufgeschlagenen Ei wälzen. Dann in die Panade geben, welche gemischt wird zu gleichen Teilen aus Semmelmehl, Parmesankäse (gerieben) und Haselnussmehl.
+			Wer will, kann sie noch mit Rosenpaprika würzen. Die panierten Schnitzel in das heiße Butterschmalz geben und von beiden Seiten schön goldgelb anbraten.
+			<br/>
+			Dem Schnitzel kann jede Beilage, welche man auch zu anderen Schnitzel zubereitet, beigegeben werden.
+			<br/>
+			Man kann auch Kalbsschnitzel statt Putenschnitzel verwenden.</p>
+	
+	</body>
+</html>	
