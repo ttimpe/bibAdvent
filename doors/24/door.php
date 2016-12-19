2506a1e7fd89affcb20fb034b2e7c252628b7a29
Georgi Reger <Georgi.Reger@bi.bib.de>
2016-12-19 19:29:40 +0100

    ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽

---
+++ doors/24/door.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<title>24. Türchen</title>
+</head>
+<body>
+<img style="width:100%;" src="iba3h14b.jpg" alt="iba3h14b">
+</body>
+</html>
